31f6d16ab94c9bf8558afa68efa3a9a1faa7f5d8
Erik <erik.sulpino.candeia@ccc.ufcg.edu.br>
2013-11-08 11:32:56 -0200

    Iniciando classe Numeros por extenso

---
+++ lab1/si1/NumeroPorExtenso.java
@@ -0,0 +1,10 @@
+package si1;
+
+public class NumeroPorExtenso {
+	
+	String[] unidades = {"zero","um","dois","tres","quatro","cinco","seis","sete","oito","nove"};
+
+	public String extenso(int numero) {
+		return unidades[numero];
+	}
+}
--- lab1/si1/TesteZeroADez.java
+++ lab1/si1/TesteZeroADez.java
@@ -2,28 +2,29 @@ package si1;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TesteZeroADez {
-	
+
+	NumeroPorExtenso ext;
+
 	@Before
 	public void setUp() throws Exception {
-		NumeroPorExtenso ext = new NumeroPorExtenso();
+		ext = new NumeroPorExtenso();
 	}
 
 	@Test
 	public void test() {
-		Assert.assertStringEquals("zero", ext.extenso("0"));
-		Assert.assertStringEquals("um", ext.extenso("1"));
-		Assert.assertStringEquals("dois", ext.extenso("2"));
-		Assert.assertStringEquals("tres", ext.extenso("3"));
-		Assert.assertStringEquals("quatro", ext.extenso("4"));
-		Assert.assertStringEquals("cinco", ext.extenso("5"));
-		Assert.assertStringEquals("seis", ext.extenso("6"));
-		Assert.assertStringEquals("sete", ext.extenso("7"));
-		Assert.assertStringEquals("oito", ext.extenso("8"));
-		Assert.assertStringEquals("nove", ext.extenso("9"));
+		Assert.assertEquals("zero", ext.extenso(0));
+		Assert.assertEquals("um", ext.extenso(1));
+		Assert.assertEquals("dois", ext.extenso(2));
+		Assert.assertEquals("tres", ext.extenso(3));
+		Assert.assertEquals("quatro", ext.extenso(4));
+		Assert.assertEquals("cinco", ext.extenso(5));
+		Assert.assertEquals("seis", ext.extenso(6));
+		Assert.assertEquals("sete", ext.extenso(7));
+		Assert.assertEquals("oito", ext.extenso(8));
+		Assert.assertEquals("nove", ext.extenso(9));
 	}
 
 }
