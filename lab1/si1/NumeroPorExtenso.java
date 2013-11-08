edf729c7f2a9aa91afa5bf58d320ffb27d3cd4a1
Erik <erik.sulpino.candeia@ccc.ufcg.edu.br>
2013-11-08 11:51:56 -0200

    Adequando classe NumeroPorExtenso aos testes dezADezenove

--- lab1/si1/NumeroPorExtenso.java
+++ lab1/si1/NumeroPorExtenso.java
@@ -2,10 +2,12 @@ package si1;
 
 public class NumeroPorExtenso {
 
-	String[] unidades = { "zero", "um", "dois", "tres", "quatro", "cinco",
-			"seis", "sete", "oito", "nove" };
+	String[] umADezenove = { "zero", "um", "dois", "tres", "quatro", "cinco",
+			"seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze",
+			"quatorze", "quize", "dezesseis", "dezessete", "dezoito",
+			"dezenove"};
 
 	public String extenso(int numero) {
-		return unidades[numero];
+		return umADezenove[numero];
 	}
 }
