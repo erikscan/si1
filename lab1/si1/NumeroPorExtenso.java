8a83c6909e7943caf8fd353a64418da1b90040ce
Erik <erik.sulpino.candeia@ccc.ufcg.edu.br>
2013-11-08 11:43:41 -0200

    Gerando testes para numeros entre dez e dezenove

--- lab1/si1/NumeroPorExtenso.java
+++ lab1/si1/NumeroPorExtenso.java
@@ -1,8 +1,9 @@
 package si1;
 
 public class NumeroPorExtenso {
-	
-	String[] unidades = {"zero","um","dois","tres","quatro","cinco","seis","sete","oito","nove"};
+
+	String[] unidades = { "zero", "um", "dois", "tres", "quatro", "cinco",
+			"seis", "sete", "oito", "nove" };
 
 	public String extenso(int numero) {
 		return unidades[numero];
--- lab1/si1/TesteZeroADez.java
+++ lab1/si1/TesteZeroADez.java
@@ -14,7 +14,7 @@ public class TesteZeroADez {
 	}
 
 	@Test
-	public void test() {
+	public void testeZeroADez() {
 		Assert.assertEquals("zero", ext.extenso(0));
 		Assert.assertEquals("um", ext.extenso(1));
 		Assert.assertEquals("dois", ext.extenso(2));
@@ -25,6 +25,19 @@ public class TesteZeroADez {
 		Assert.assertEquals("sete", ext.extenso(7));
 		Assert.assertEquals("oito", ext.extenso(8));
 		Assert.assertEquals("nove", ext.extenso(9));
+	}
+	@Test
+	public void testeOnzeADezenove(){
+		Assert.assertEquals("onze", ext.extenso(11));
+		Assert.assertEquals("doze", ext.extenso(12));
+		Assert.assertEquals("treze", ext.extenso(13));
+		Assert.assertEquals("quatorze", ext.extenso(14));
+		Assert.assertEquals("quize", ext.extenso(15));
+		Assert.assertEquals("dezesseis", ext.extenso(16));
+		Assert.assertEquals("dezessete", ext.extenso(17));
+		Assert.assertEquals("dezoito", ext.extenso(18));
+		Assert.assertEquals("dezenove", ext.extenso(19));
+		Assert.assertEquals("dez", ext.extenso(10));	
 	}
 
 }
