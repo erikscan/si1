9ab8746266b8d2fb55869d3dbce2a099fdd89e15
Erik <erik.sulpino.candeia@ccc.ufcg.edu.br>
2013-11-08 12:18:06 -0200

    Modelando NumeroPorExtenso pelo testeDezenas

--- lab1/si1/NumeroPorExtenso.java
+++ lab1/si1/NumeroPorExtenso.java
@@ -5,9 +5,15 @@ public class NumeroPorExtenso {
 	String[] umADezenove = { "zero", "um", "dois", "tres", "quatro", "cinco",
 			"seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze",
 			"quatorze", "quize", "dezesseis", "dezessete", "dezoito",
-			"dezenove"};
+			"dezenove" };
+
+	String[] dezenas = { "", "", "vinte", "trinta", "quarenta", "cinquenta",
+			"sessenta", "setenta", "oitenta", "noventa" };
 
 	public String extenso(int numero) {
-		return umADezenove[numero];
+		if (numero < 20)
+			return umADezenove[numero];
+		return dezenas[(numero / 10)];
+
 	}
 }
