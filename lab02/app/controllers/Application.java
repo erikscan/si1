51b8225d53454cd6b12941e980ae5f77be97d73e
Erik <erik.sulpino.candeia@ccc.ufcg.edu.br>
2013-11-26 10:00:48 -0200

    Criando tasks

--- lab02/app/controllers/Application.java
+++ lab02/app/controllers/Application.java
@@ -1,14 +1,24 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok("Hello World !");
-    }
+	public static Result index() {
+		return ok("Hello World !");
+	}
+
+	public static Result tasks() {
+		return TODO;
+	}
+
+	public static Result newTask() {
+		return TODO;
+	}
+
+	public static Result deleteTask(Long id) {
+		return TODO;
+	}
 
 }
